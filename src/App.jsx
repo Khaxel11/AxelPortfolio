91c3e866617d59282723a817d79dd29c9bc60a48
Khaxel11 <agui.axel11@gmail.com>
2024-03-15 01:20:35 -0700

    Init Commit
    Starting REACT Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+const App = () => {
+  return <div>
+    <section id="HomePage">
+      <Navbar/>
+      <Hero/>
+    </section>;
+    <section id="Services">Parallax</section>;
+    <section>Services</section>;
+    <section id="Portfolio">Parallax</section>;
+    <section>Portfolio1</section>;
+    <section>Portfolio2</section>;
+    <section id="Contact">Portfolio3</section>;
+    <section>Contact</section>;
+  </div>;
+};
+
+export default App;
