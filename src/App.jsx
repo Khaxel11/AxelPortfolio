3bf18088633a2e817deed74e7201c60c1b37c2b3
Axel Aguilar Alonso <agui.axel11@gmail.com>
2024-03-19 16:03:16 -0700

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+import Tooling from "./components/tooling/Tooling";
 const App = () => {
   return <div>
     <section id="HomePage">
-      <Navbar/>
-      <Hero/>
-    </section>;
-    <section id="Services">Parallax</section>;
-    <section>Services</section>;
-    <section id="Portfolio">Parallax</section>;
-    <section>Portfolio1</section>;
-    <section>Portfolio2</section>;
-    <section id="Contact">Portfolio3</section>;
-    <section>Contact</section>;
+      <Navbar />
+      <Hero />
+    </section>
+    <section id="Services">
+      <Parallax type="services" />
+    </section>
+    <section>
+      <Services />
+    </section>
+    <section>
+      <Tooling />
+    </section>
+    <section id="Portfolio">
+      <Parallax type="portfolio" />
+    </section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section id="Contact">Portfolio3</section>
+    <section>Contact</section>
   </div>;
 };
 
