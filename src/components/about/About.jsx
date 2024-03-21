a22d12c0db0a38c898af137bcab05843271019e7
Axel Aguilar Alonso <agui.axel11@gmail.com>
2024-03-21 12:46:56 -0700

    About
    Finish About Me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,8 @@
 import './about.scss'
 import { useState, useEffect, useRef } from 'react';
 import { motion, useInView, stagger } from 'framer-motion'
-
+import { Spotify } from 'react-spotify-embed'
+const spotifyURL= "https://open.spotify.com/intl-es/track/0wEGo9LsSrSUz9HOtraWAO?si=9d69e97c7cdc4f54";
 const imgVariants = {
   initial: {
     y: 0,
@@ -48,8 +49,8 @@ const descriptionVariants = {
   },
 };
 const About = () => {
-  
-  
+
+
   const text = "Hello I Am Axel".split("");
   const [letterIndex, setLetterIndex] = useState(0);
   const [isTyping, setIsTyping] = useState(true);
@@ -63,7 +64,7 @@ const About = () => {
   }, []);
 
   useEffect(() => {
-    if (letterIndex >= text.length ) {
+    if (letterIndex >= text.length) {
       // setIsTyping(false);
     }
   }, [letterIndex, text]);
@@ -74,64 +75,65 @@ const About = () => {
   };
   const variants = {
     initial: {
-        opacity: 0,
+      opacity: 0,
     },
-    animate:{
-      
-        opacity : 1,
-        transition:{
-            duration : 1,
-            staggerChildren: 1,
-        },
-  
+    animate: {
+
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 1,
+      },
+
     }
   }
   const ref = useRef()
-  const isInView = useInView(ref, {margin:"0px"})
+  const isInView = useInView(ref, { margin: "0px" })
   return (
-    <motion.div className='about' ref={ref}  variants={variants} initial="initial" animate={isInView && "animate"}>
+    <motion.div className='about' ref={ref} variants={variants} initial="initial" animate={isInView && "animate"}>
+
       <motion.div className="imageContainer" variants={imgVariants} initial="initial" animate={isInView && "animate"} >
         <motion.img src="/Laptop.png" alt="" />
       </motion.div>
       {isInView && (
-         <motion.div className="nameContainer" variants={textVariants} initial="initial"  animate={isInView && "animate"} >
-         {text.slice(0, letterIndex).map((el, i) => (
-           <motion.span
-             initial={{ opacity: 0 }}
-             animate={isInView && { opacity: 1 }}
-             transition={isInView &&{
-               duration: 0.75,
-               delay: i / 45,
-             }}
-             key={i}
-             style={{ color: i >= 10 ? 'orange' : '' }}
- 
-           >
-             <b >{el}{" "}</b>
-           </motion.span>
-         ))}
-         {(
-           <motion.span
-             variants={cursorVariants}
-             initial="hidden"
-             animate={isInView && { opacity: [0, 1, 0] }}
-             transition={{ duration: 0.9, repeat: Infinity }}
-             style={{ display: 'inline-block', marginLeft: '5px' }}
-           >
-             |
-           </motion.span>
-         )}
-         {letterIndex >= text.length ? (
-           <motion.h5
-             initial={{ opacity: 0 }}
-             animate={isInView &&{ opacity: 1 }}
-             transition={{
-               duration: 0.75,
- 
-             }}
-           >Axel Aguilar</motion.h5>
-         ) : null}
-       </motion.div>
+        <motion.div className="nameContainer" variants={textVariants} initial="initial" animate={isInView && "animate"} >
+          {text.slice(0, letterIndex).map((el, i) => (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={isInView && { opacity: 1 }}
+              transition={isInView && {
+                duration: 0.75,
+                delay: i / 45,
+              }}
+              key={i}
+              style={{ color: i >= 10 ? 'orange' : '' }}
+
+            >
+              <b >{el}{" "}</b>
+            </motion.span>
+          ))}
+          {(
+            <motion.span
+              variants={cursorVariants}
+              initial="hidden"
+              animate={isInView && { opacity: [0, 1, 0] }}
+              transition={{ duration: 0.9, repeat: Infinity }}
+              style={{ display: 'inline-block', marginLeft: '5px' }}
+            >
+              |
+            </motion.span>
+          )}
+          {letterIndex >= text.length ? (
+            <motion.h5
+              initial={{ opacity: 0 }}
+              animate={isInView && { opacity: 1 }}
+              transition={{
+                duration: 0.75,
+
+              }}
+            >Axel Aguilar</motion.h5>
+          ) : null}
+        </motion.div>
       )}
       {letterIndex >= text.length ? (
         <motion.div className="descriptionContainer" variants={descriptionVariants} initial="initial" animate={isInView && "animate"}>
@@ -147,30 +149,33 @@ const About = () => {
         </motion.div>
 
       ) : null}
-      
-      <motion.div className="essentialsContainer" initial="hidden" animate="visible" whileHover={{ scale: 1.2, rotate: 5, transition: { duration: 0.3 } }}  whileTap={{ scale: 0.9 }} >
-      <motion.img
-        src="/circle.svg"
-        alt=""
-        className="circle"
-        animate={{ rotate: [0, -5, 0, 5, 0], transition: { duration: 0.5, repeat: Infinity } }}
-      />
-      <p className="text">essentials</p>
-      <motion.img
-        src="/arrow.svg"
-        alt=""
-        className="arrow"
-        animate={{ rotate: [0, -5, 0, 5, 0], transition: { duration: 0.5, repeat: Infinity } }}
-      />
-      <div className="items">
-        <motion.img className='icons' src="/spotify.svg" alt=""  whileHover={{ scale: 1.2}}/>
-        <motion.img className='icons' src="/coffee.svg" alt="" whileHover={{ scale: 1.2}} />
+
+      <motion.div className="essentialsContainer" initial="hidden" animate="visible" whileHover={{ scale: 1.2, rotate: 5, transition: { duration: 0.3 } }} whileTap={{ scale: 0.9 }} >
+        <motion.img
+          src="/circle.svg"
+          alt=""
+          className="circle"
+          animate={{ rotate: [0, -5, 0, 5, 0], transition: { duration: 0.5, repeat: Infinity } }}
+        />
+        <p className="text">essentials</p>
+        <motion.img
+          src="/arrow.svg"
+          alt=""
+          className="arrow"
+          animate={{ rotate: [0, -5, 0, 5, 0], transition: { duration: 0.5, repeat: Infinity } }}
+        />
+        {/* <div className="items">
+          <motion.img className='icons' src="/spotify.svg" alt="" whileHover={{ scale: 1.2 }} />
+          <motion.img className='icons' src="/coffee.svg" alt="" whileHover={{ scale: 1.2 }} />
+        </div> */}
+
+      </motion.div>
+      <div className="essentialsItems">
+        <label className='hoverEffect'><a href={spotifyURL}>actual favorite song</a></label>
+        <Spotify wide link={spotifyURL}/>
       </div>
-      
     </motion.div>
-    
-    </motion.div>
-    
+
   );
 }
 
